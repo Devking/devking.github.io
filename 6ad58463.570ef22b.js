62f8718cddc4d14c2a43e96a9587f7e239eabdff
Wells Lucas Santo <theonlydevking@gmail.com>
2021-01-05 02:51:43 -0800

    Deploy website - based on 671c12b6427df16edcf7e8e1ea2714b27d275415

---
+++ 6ad58463.570ef22b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{77:function(t){t.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Testing Things Out","permalink":"/blog/testing-things-out"}]}')}}]);
